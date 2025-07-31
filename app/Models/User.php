df961ff1a85b09a7857ea852c998707d001679ab
umidbeksalayev <umidbeksalayev13@gmail.com>
2025-07-31 21:02:07 +0500

    UserAccount model create

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,10 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+    public function account()
+    {
+        return $this->hasOne(UserAccount::class);
+    }
 }
